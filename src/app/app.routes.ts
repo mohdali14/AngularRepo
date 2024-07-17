3cc491c073762ce0eb5bf6e8f04a073e309ebc99
Mohammed Ali Shaik <MohammedAli_Shaik@ajg.com>
2024-07-17 14:04:08 -0500

    routing learn php site and added document

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
 
-    {'path':'', component: HomeComponent},
-    {'path':'about', component: AboutComponent},
-    {'path':'contact', component: ContactComponent},
+    {'path':'home', 'title' :'Home', component: HomeComponent},
+    {'path':'about','title' :'About', component: AboutComponent},
+    {'path':'contact', 'title' :'Contact', component: ContactComponent},
+     {'path':'', redirectTo:'/home', pathMatch:'full'},
+    {'path':'**', component: PageNotFoundComponent},
 ];
